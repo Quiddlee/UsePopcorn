03eae48bfb5a7fb7c7ee4a5e546214767687b037
Bogdan <thegarrybit@gmail.com>
2023-07-11 14:12:54 +0300

    feat: add star rating component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="purple"
+        onSetRatingHandler={setMovieRating}
+      />
+      <p>This movie rated by {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating
+      size={24}
+      color="orangered"
+      className="test"
+      defaultRation={3}
+    />
+    <Test />
   </React.StrictMode>,
 );
