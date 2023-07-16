3ef09c1599f315f3f08552107c27087902a0289a
Bogdan <thegarrybit@gmail.com>
2023-07-16 12:17:48 +0300

    refactor: change api constant

--- src/config.js
+++ src/config.js
@@ -1,1 +1,7 @@
 export const API_KEY = 'dbb72d83';
+export const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+export const API_SEARCH_BY_NAME_POSTFIX = '&s=';
+export const API_SEARCH_BY_ID_POSTFIX = '&i=';
+
+export const API_SEARCH_NAME = `${API_URL}${API_SEARCH_BY_NAME_POSTFIX}`;
+export const API_SEARCH_ID = `${API_URL}${API_SEARCH_BY_ID_POSTFIX}`;
