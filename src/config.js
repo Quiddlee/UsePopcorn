403b3d78c91b2adf2a18ba00a51089b7f417967a
Bogdan <thegarrybit@gmail.com>
2023-07-31 11:28:27 +0300

    feat: deploy

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const API_KEY = 'dbb72d83';
-export const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
-export const API_SEARCH_BY_NAME_POSTFIX = '&s=';
-export const API_SEARCH_BY_ID_POSTFIX = '&i=';
-
-export const API_SEARCH_NAME = `${API_URL}${API_SEARCH_BY_NAME_POSTFIX}`;
-export const API_SEARCH_ID = `${API_URL}${API_SEARCH_BY_ID_POSTFIX}`;
-export const IMAGE_NOT_FOUND = 'N/A';
-export const LOCAL_STORAGE_WATCHED = 'watched';
